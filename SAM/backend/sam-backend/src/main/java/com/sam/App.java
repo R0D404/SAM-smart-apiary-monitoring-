6550ca663d047afd9e9dffcd5c9d2fa7fc26dc67
R0D404 <rodrgit1906@gmail.com>
2026-07-09 22:21:46 -0600

    first comit

---
+++ SAM/backend/sam-backend/src/main/java/com/sam/App.java
@@ -0,0 +1,13 @@
+package com.sam;
+
+import io.javalin.Javalin;
+import com.sam.controladores.AuthControlador;
+
+import static io.javalin.apibuilder.Apibuilder.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
